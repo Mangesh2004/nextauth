bb74262e7f49b0508bb14ebc67d1e6e6af4528fb
Mangesh2004 <mangeshwaybhase01@gmail.com>
2024-08-07 01:00:35 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import authConfig from "./auth.config";
 import NextAuth from "next-auth";
+
 import {
   DEFAULT_LOGIN_REDIRECT,
   apiAuthPrefix,
@@ -7,38 +8,36 @@ import {
   authRoutes
 } from "@/routes"
 
-const { auth } = NextAuth(authConfig);
-
+const {auth} = NextAuth(authConfig)
+ 
 export default auth((req) => {
-  const { nextUrl } = req;
-  const isLoggedin = !!req.auth;
+  const {nextUrl}=req;
+  const isLoggedin=!!req.auth;
 
-  const isApiAuthroute = nextUrl.pathname.startsWith(apiAuthPrefix);
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isApiAuthroute=nextUrl.pathname.startsWith(apiAuthPrefix)
+  const isPublicRoute=publicRoutes.includes(nextUrl.pathname)
+  const isAuthRoute=authRoutes.includes(nextUrl.pathname)
 
   if (isApiAuthroute) {
-    return new Response(null, { status: 200 }); // or any appropriate response
+    return new Response(null)
   }
 
   if (isAuthRoute) {
     if (isLoggedin) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT,nextUrl))
     }
-    return new Response(null, { status: 401 }); // or any appropriate response
+    return new Response(null)
   }
 
   if (!isLoggedin && !isPublicRoute) {
-    return Response.redirect(new URL("/auth/login", nextUrl));
+    return Response.redirect(new URL("/auth/login",nextUrl))
   }
-
-  return new Response(null, { status: 200 }); // or any appropriate response
-});
-
+  
+})
+ 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)'
-  ],
-};
+  matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    
+    '/(api|trpc)(.*)'],
+}
